3a8b92f9efc01abf6d54126c919fd55a64f05b46
Jimmyelvis <jinjoe@zoho.com>
2018-06-06 17:07:32 -0500

    added database creditials

--- config/config.php
+++ config/config.php
@@ -1,10 +1,12 @@
 <?php
 ob_start(); //Turns on output buffering
 session_start();
+require 'config/keys.php';
 
 $timezone = date_default_timezone_set("America/Denver");
 
-$con = mysqli_connect("localhost", "root", "", "swirl"); //Connection variable
+// $con = mysqli_connect("localhost", "root", "", "swirl"); //Connection variable
+$con = mysqli_connect("sql9.freemysqlhosting.net", "sql9241732", "aZRMPi3MXU", "sql9241732");
 
 if(mysqli_connect_errno())
 {
--- register.php
+++ register.php
@@ -11,7 +11,7 @@ require 'includes/form_handlers/login_handler.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Welcome To Swirl Feed</title>
+  <title>Welcome To Social Cuisiner</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="assets/css/register_styles.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
