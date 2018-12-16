4c166c9aa079213573242af19dd6e0efd797b620
Jimmyelvis <jinjoe@zoho.com>
2018-12-16 18:09:17 -0500

    Changed readme file

--- config/config.php
+++ config/config.php
@@ -6,7 +6,9 @@ session_start();
 $timezone = date_default_timezone_set("America/Denver");
 
 // $con = mysqli_connect("localhost", "root", "", "swirl"); //Connection variable
-$con = mysqli_connect("db4free.net", "jinjoe", "panther69", "social");
+// $con = mysqli_connect("db4free.net", "jinjoe", "panther69", "social");
+$con = mysqli_connect("mlmontheweb.com", "jinjoe", "Panther69!", "social-2");
+
 
 if(mysqli_connect_errno())
 {
